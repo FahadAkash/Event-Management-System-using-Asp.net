ed4860c8a06f74b8f3ea3fc7f63a2f9977465d2f
FahadAkash <fahad12345ff8@gmail.com>
2025-01-07 19:47:00 +0600

    modified somthing

--- EventManager/event-management-system/event-management-system.Server/obj/Debug/net8.0/event-management-system.Server.AssemblyInfo.cs
+++ EventManager/event-management-system/event-management-system.Server/obj/Debug/net8.0/event-management-system.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("event-management-system.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b958e7fa31ca3b20c53b3d22a5b52a22740e7c67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef366fb9691f3a36205af60e7baf9d340019bbcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("event-management-system.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("event-management-system.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
